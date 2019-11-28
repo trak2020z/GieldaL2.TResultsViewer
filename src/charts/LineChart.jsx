6084d2c981e8d48461193aa82efb9554e8b4a72f
TheZephyr <slodownik.maciej@gmail.com>
2019-11-28 16:16:49 +0100

    Fix theme toggle and change some styling

--- src/charts/LineChart.jsx
+++ src/charts/LineChart.jsx
@@ -1,43 +1,48 @@
-import React from 'react';
-import Chart from 'chart.js';
+import React from "react";
+import Chart from "chart.js";
 // eslint-disable-next-line
-import ChartDataLabels from 'chartjs-plugin-datalabels'
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
-/** Graphs a chart using connected points  
+/** Graphs a chart using connected points
  * Available props:
  * - data (Datasets to plot)
+ * - chartTitle (Chart title)
  * - title1 (1st dataset title)
  * - title2 (2nd dataset title)
  * - xlabel (X axis label)
  * - ylabel (Y axis label)
-*/
+ * - textColor (Font color)
+ */
 class LineChart extends React.Component {
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
-    Chart.defaults.global.defaultFontColor = 'white';
+    Chart.defaults.global.defaultFontColor = this.props.textColor;
     Chart.defaults.global.defaultFontFamily = "'Roboto', sans-serif";
   }
 
   /** Sets up new line Chart (using chart.js lib) on component mount using set props */
   componentDidMount() {
     this.myChart = new Chart(this.canvasRef.current, {
-      type: 'line',
+      type: "line",
       data: {
         labels: this.props.data.map(d => d.testStartTime),
-        datasets: [{
-          label: this.props.title1,
-          backgroundColor: '#006064',
-          borderColor: '#006064',
-          data: this.props.data.map(d => d.reqTime),
-          fill: false
-        }, {
-          label: this.props.title2,
-          backgroundColor: '#00BCD4',
-          borderColor: '#00BCD4',
-          data: this.props.data.map(d => d.backendTime),
-          fill: false
-        }]
+        datasets: [
+          {
+            label: this.props.title1,
+            backgroundColor: "#006064",
+            borderColor: "#006064",
+            data: this.props.data.map(d => d.reqTime),
+            fill: false
+          },
+          {
+            label: this.props.title2,
+            backgroundColor: "#00BCD4",
+            borderColor: "#00BCD4",
+            data: this.props.data.map(d => d.backendTime),
+            fill: false
+          }
+        ]
       },
       options: {
         maintainAspectRatio: false,
@@ -45,63 +50,68 @@ class LineChart extends React.Component {
         title: {
           display: true,
           fontSize: 20,
-          fontColor: 'white',
-          text: this.props.chartTitle
+          text: this.props.chartTitle,
+          fontColor: this.props.textColor
         },
         hover: {
-          mode: 'nearest',
+          mode: "nearest",
           intersect: true
         },
         tooltips: {
-          mode: 'index',
+          mode: "index",
           intersect: false
         },
         scales: {
-          xAxes: [{
-            display: true,
-            scaleLabel: {
+          xAxes: [
+            {
               display: true,
-              labelString: this.props.xlabel
+              scaleLabel: {
+                display: true,
+                labelString: this.props.xlabel
+              }
             }
-          }],
-          yAxes: [{
-            display: true,
-            scaleLabel: {
+          ],
+          yAxes: [
+            {
               display: true,
-              labelString: this.props.ylabel
+              scaleLabel: {
+                display: true,
+                labelString: this.props.ylabel
+              }
             }
-          }]
+          ]
         },
-        plugins:{
+        plugins: {
           datalabels: {
-            anchor: 'center',
+            anchor: "center",
             font: {
-              size: 10,
-              weight: 'bolder'
+              size: 8,
+              weight: "bolder"
             },
             backgroundColor: function(context) {
-              return context.dataset.backgroundColor
+              return context.dataset.backgroundColor;
             },
             borderRadius: 10,
-            color: 'white',
-            clamp: 'true',
-            display: 'auto'
-          },
-          generateLabels: function(myChart) {
-            myChart.legend.afterFit = function() {
-              this.height = this.height + 50;
-            }
-          },
+            color: this.props.textColor,
+            clamp: "true",
+            display: "auto"
+          }
         }
       }
     });
   }
 
   /** Refresh chart properties on every component update */
-  componentDidUpdate() {
+  componentDidUpdate(prevProps) {
+    // TODO: https://pl.reactjs.org/docs/react-component.html#componentdidupdate
     this.myChart.data.labels = this.props.data.map(d => d.testStartTime);
     this.myChart.data.datasets[0].data = this.props.data.map(d => d.reqTime);
-    this.myChart.data.datasets[1].data = this.props.data.map(d => d.backendTime);
+    this.myChart.data.datasets[1].data = this.props.data.map(
+      d => d.backendTime
+    );
+    if (this.props.textColor !== prevProps.textColor) {
+      this.myChart.options.title.fontColor = this.props.textColor;
+    }
     this.myChart.update();
   }
 
